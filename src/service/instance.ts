18a3985ca0be234a6d7898cc5c863a58af91e309
juviaLocker <13064366610@163.com>
2020-08-06 09:22:58 +0800

    后台报错提示状态码,导出表格的方法

--- src/service/instance.ts
+++ src/service/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_URL, TOKEN_KEY } from '../config';
 import { isTokenExpired } from '../helpers/utils';
-import iView,{Modal} from 'iview';
+import iView, { Modal } from 'iview';
 import 'iview/dist/styles/iview.css';
 import { Vue } from 'vue-property-decorator';
 import router from 'vue-router';
@@ -31,7 +31,7 @@ export const getToken = () => {
         token = window.sessionStorage.getItem(TOKEN_KEY)!;
       }
     } catch (e) {
-      token = 'token';
+      // token = 'token';
     }
     if (!token) { token = 'token'; }
     if (token && !isTokenExpired(token)) {
@@ -47,16 +47,17 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
   baseURL: API_URL,
-  timeout: 20000,
+  timeout: 60000,
   // method: 'post',
 });
 
-// request interceptor to pass auth token
 // tslint:disable-next-line:no-any
 axiosInstance.interceptors.request.use(async (config: any) => {
   try {
     const token = await getToken();
-    if (config.url !== '/distribution/agencyUserLogin' && config.url !== '/api/public/sendMessage') {
+    // tslint:disable-next-line:max-line-length
+    // 用于后台没有发送token的情况
+    if (config.url !== '/api/public/adminLogin') {
       config.headers.acto = `${token}`;
     }
     return config;
@@ -76,42 +77,45 @@ axiosInstance.interceptors.response.use((response) => {
     return response;
   }
   if (response.data.rtnCode === 401) {
-    store.dispatch('rmToken');
+    
     // TODO:退出登录
     location.href = '/login';
     // window.location.href = response.config.baseURL!;// 该方式只适用于前后端一起部署
   }
   return response;
-// tslint:disable-next-line:only-arrow-functions
-},function(error) {
+}, function (error) {
   // 对响应错误做点什么
-  if (error.response.status === 401) {
-    // tslint:disable-next-line:no-any
+  if (error.response && error.response.status === 401) {
     (Modal as any).warning({
       title: '警告',
-      content: '<p>您的账号在其他地方登录或登录超时,请注意账号安全</p>',
+      content: '<p>您的账号在其他地方登录或登录超时，请注意账号安全！</p>',
       onOk: () => {
         location.href = '/';
       },
-      // tslint:disable-next-line:no-empty
       onCancel: () => {
       },
     });
-  } else if (error.response.status !== 500) {
-    // tslint:disable-next-line:no-any
+  } else if (error.response && error.response.status !== 401) {
     (Modal as any).warning({
       title: '警告',
-      content: '<p>后台网络错误,请稍后再试</p>',
-      // tslint:disable-next-line:no-empty
+      content: `<p>后台网络错误${error.response.status}，请稍后再试！</p>`,
       onOk: () => {
       },
-      // tslint:disable-next-line:no-empty
+      onCancel: () => {
+      },
+    });
+  }else {
+    (Modal as any).warning({
+      title: '警告',
+      content: `<p>后台网络错误，请稍后再试！</p>`,
+      onOk: () => {
+      },
       onCancel: () => {
       },
     });
   }
+  console.log(error);
   return Promise.reject(error);
-// tslint:disable-next-line:only-arrow-functions
 });
 
 export default axiosInstance;
